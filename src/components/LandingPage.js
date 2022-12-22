533e274066dc9dff53c4a8786d0850409d12769f
Ntawuyirushintege Patrick <ntawuyirushintegepatrick@gmail.com>
2022-12-22 16:59:24 +0200

    making all design

---
+++ src/components/LandingPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="Header">
+        <div className="submenu">bnfbngnbgfbnb</div>
+        <div className="profile-menu">dfgffbhfgbhfgb</div>
+      </div>
+      <div className="Header-two">fghghf</div>
+      <div className="categorie">dfvfgfgd</div>
+      <div className="welcome-photo">
+        <div className="welcome">fbfbfbfbfbf</div>
+        <div className="photo">fgfgbfgbhfgbf</div>
+      </div>
+      <div className="categories">
+        <div className="cat-cat-see-all">
+          <div className="cat">effvf</div>
+          <div className="cat-see-all">effvf</div>
+        </div>
+        <div className="categories-with-photos">sdfjdfjbd</div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
