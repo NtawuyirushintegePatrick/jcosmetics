1fbda832af1df47e06b6e961fefb870e8901038d
Ntawuyirushintege Patrick <ntawuyirushintegepatrick@gmail.com>
2022-12-23 09:45:40 +0200

    working on header design

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,8 +4,36 @@ const LandingPage = () => {
   return (
     <>
       <div className="Header">
-        <div className="submenu">bnfbngnbgfbnb</div>
-        <div className="profile-menu">dfgffbhfgbhfgb</div>
+        <div className="submenu">
+          <div className="submenu-list">
+            <a
+              href="#"
+              class="text-blue-600 visited:text-purple-600 no-underline"
+            >
+              Daily Deal
+            </a>
+            <a href="#" class="text-blue-600 visited:text-purple-600">
+              Sell
+            </a>
+            <a href="#" class="text-blue-600 visited:text-purple-600">
+              Contact us
+            </a>
+            <a href="#" class="text-blue-600 visited:text-purple-600">
+              About us
+            </a>
+            <a href="#" class="text-blue-600 visited:text-purple-600">
+              Help
+            </a>
+          </div>
+        </div>
+        <div className="profile-menu">
+          <a href="#" class="text-blue-600 visited:text-purple-600">
+            Register
+          </a>
+          <a href="#" class="text-blue-600 visited:text-purple-600">
+            Sign in
+          </a>
+        </div>
       </div>
       <div className="Header-two">fghghf</div>
       <div className="categorie">dfvfgfgd</div>
@@ -19,6 +47,21 @@ const LandingPage = () => {
           <div className="cat-see-all">effvf</div>
         </div>
         <div className="categories-with-photos">sdfjdfjbd</div>
+      </div>
+      <div className="products">
+        <div className="pro-see-all">
+          <div className="pro">dfdd</div>
+          <div className="search-product">fgdfg</div>
+          <div className="see-all">fdgfg</div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="stay-connected">fdfdf</div>
+        <div className="contact-us">dfdds</div>
+      </div>
+      <div className="footer-privacy-managment">
+        <div className="privacy">fdfdf</div>
+        <div className="managment">dfdds</div>
       </div>
     </>
   );
